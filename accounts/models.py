9749bc91fcd4d6b6da9ca0967fab7c032ff81edf
Arnob Chowdhury <chowdhury.sami.al@gmail.com>
2023-03-10 07:37:29 +0600

    Create company

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import User
-from django.db.models import (Model, CharField, ForeignKey, CASCADE, OneToOneField)
+from django.db.models import (Model, CharField, ForeignKey, CASCADE, OneToOneField, BooleanField)
 
 
 class Company(Model):
-    name = CharField(max_length=255)
+    name = CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
         return self.name
@@ -14,6 +14,7 @@ class Employee(Model):
     designation = CharField(max_length=255)
     company = ForeignKey(Company, on_delete=CASCADE, related_name="company")
     user = OneToOneField(User, on_delete=CASCADE)
+    is_admin = BooleanField(default=False)
 
     def __str__(self) -> str:
         return f"{self.user.username}, {self.department} {self.designation}, {self.company.name}"
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import UserRegisterView
+from accounts.views import UserRegisterView, CreateCompanView
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user_registration'),
+    path('create-company/', CreateCompanView.as_view(), name='create_company'),
 ]
